58e0330e89601d339b6426bdcda5008c153ae881
Karim Abou Zeid <karim23697@gmail.com>
2019-06-27 15:03:15 +0200

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Charts",
     products: [
-        .library(name: "Charts", type: .dynamic, targets: ["Charts"])
+        .library(name: "Charts", targets: ["Charts"])
     ],
     dependencies: [],
     targets: [
         .target(name: "Charts", dependencies: [])
-    ],
-    swiftLanguageVersions: [5]
+    ]
 )
